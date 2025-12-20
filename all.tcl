31cfa83789c8dde3ce8fd17a8b5d904580512d89
Peter Lewerin <plewerin@gmail.com>
2025-12-20 07:00:27 +0100

    Update all.tcl

--- all.tcl
+++ all.tcl
@@ -1,7 +1,19 @@
 package require Tcl
 package require tcltest
 
-::tcltest::configure -testdir \
-        [file dirname [file normalize [info script]]]
-eval ::tcltest::configure $argv
+set dir [file dirname [file normalize [info script]]]
+
+::tcltest::matchFiles ../constcl.test
+::tcltest::configure -testdir [file join $dir tests]
+::tcltest::configure -tmpdir [file join $dir tmp]
+set OUTFILE [file join $dir tmp outfile.txt]
+set ERRFILE [file join $dir tmp errfile.txt]
+exec echo -n "" > $OUTFILE
+exec echo -n "" > $ERRFILE
+::tcltest::configure -outfile $OUTFILE
+::tcltest::configure -errfile $ERRFILE
+
+::tcltest::configure -verbose {body start error line}
+
+::tcltest::configure {*}$argv
 ::tcltest::runAllTests
