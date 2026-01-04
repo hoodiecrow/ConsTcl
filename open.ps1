d883c8c862edca33d34f649141234aa330008054
Peter Lewerin <plewerin@gmail.com>
2026-01-04 07:21:59 +0100

    Create open.ps1

---
+++ open.ps1
@@ -0,0 +1,27 @@
+# Source - https://superuser.com/a/1896742
+# Posted by Vomit IT - Chunky Mess Style, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-01-03, License - CC BY-SA 4.0
+
+$folders = "C:\Users\plewe\Documents\Github",
+	"C:\Users\plewe\Documents\GitHub\ConsTcl",
+	"C:\Users\plewe\Documents\GitHub\ConsTcl\tests",
+	"C:\Users\plewe\Documents\GitHub\ConsTcl\tmp";
+
+$wshell = New-Object -ComObject WScript.Shell;
+
+Start-Process "explorer.exe" "Documents";
+Start-Sleep -Milliseconds 500; 
+
+$wshell.AppActivate("Documents");
+Start-Sleep -Seconds 1;
+
+$wshell.SendKeys("%");     # Alt
+$wshell.SendKeys("{TAB}"); # TAB
+
+$folders | ForEach-Object {
+    Start-Sleep -Milliseconds 500; 
+    $wshell.SendKeys("^{t}");  # Ctrl+T
+    Start-Sleep -Milliseconds 500; 
+    $wshell.SendKeys("%d");    # Alt+D
+    $wshell.SendKeys("$_{ENTER}");
+    };
