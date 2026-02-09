91fa4e8d84507219a4abcbdf46c57d5c5e7a08e4
Peter Lewerin <plewerin@gmail.com>
2026-02-09 21:10:03 +0100

    Update all.tcl

--- all.tcl
+++ all.tcl
@@ -4,11 +4,11 @@ namespace import -force ::tcltest::*
 
 set dir [file dirname [file normalize [info script]]]
 
-matchFiles ../constcl.test
 configure -testdir [file join $dir tests]
-configure -tmpdir [file join $dir tmp]
-set OUTFILE [file join $dir tmp outfile.txt]
-set ERRFILE [file join $dir tmp errfile.txt]
+configure -tmpdir [file join $dir tests tmp]
+#matchFiles ../constcl.test
+set OUTFILE [file join $dir tests tmp outfile.txt]
+set ERRFILE [file join $dir tests tmp errfile.txt]
 exec echo -n "" > $OUTFILE
 exec echo -n "" > $ERRFILE
 configure -outfile $OUTFILE
