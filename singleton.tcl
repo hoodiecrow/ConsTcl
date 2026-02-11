0defee7877191dc6668c2baf8eb113d0d0c4181f
Peter Lewerin <plewerin@gmail.com>
2026-02-11 03:41:00 +0100

    Delete singleton.tcl

--- singleton.tcl
+++
@@ -1,9 +0,0 @@
-proc makesingleton {NAME TEXT} {
-    oo::object create ::$NAME
-    oo::objdefine ::$NAME {
-        class ::constcl::Base  
-        method new {} {return [self]}
-        unexport create destroy
-    }
-    oo::objdefine ::$NAME method tstr {} [list return $TEXT]
-}
