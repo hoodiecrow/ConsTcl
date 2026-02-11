ea358eaea44d9cc7db3cd48cbe139ed7df8fe88f
Peter Lewerin <plewerin@gmail.com>
2026-02-11 03:40:37 +0100

    Delete transformations.tcl

--- transformations.tcl
+++
@@ -1,7 +0,0 @@
-%s/\${::#EOF}/[::EndOfFile new]/gc
-%s/#EOF/[::EndOfFile new]/gc
-%s/\${::#t}/[::True new]/gc
-%s/\${::#f}/[::False new]/gc
-%s/\${::#NIL}/[::EmptyList new]/gc
-%s/\${::#UND}/[::Undefined new]/gc
-%s/\${::#UNS}/[::Unspecified new]/gc
